de33b0ea24a75285e67a543de44c474356fcd38b
Eric Leo <lsly.eric@gmail.como>
2019-04-13 21:05:26 +0800

    20190413 submit code

--- app/components/rental-listing.js
+++ app/components/rental-listing.js
@@ -3,8 +3,10 @@ import Component from '@ember/component';
 export default Component.extend({
     isWide: false,
     actions: {
-      toggleImageSize() {
-        this.toggleProperty('isWide');
-      }
+        toggleImageSize() {
+            //toggleProperty方法直接把isShowingBody设置为相反值
+            //toggleProperty方法详情：http://devdocs.io/ember/classes/ember.observable#method_toggleProperty
+            this.toggleProperty('isWide');
+        }
     }
 });
